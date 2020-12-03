5925a5499f0a83fe8f9c856a84e662ee73071171
ramibakir <rami.bakir@hotmail.no>
2020-12-03 19:09:13 +0100

    Setup articledetailedview with styling and dummy data, made route for it to debug styling

--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import {
   StyledImage,
   StyledParagraph,
@@ -8,13 +7,6 @@ import {
   StyledButton,
 } from '../styles/MainStyles';
 import { StyledListContainer, StyledListItem } from '../styles/ListStyles';
-
-const TempHeader = styled(StyledHeader)`
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 const TempContainer = styled.div`
   display: flex;
