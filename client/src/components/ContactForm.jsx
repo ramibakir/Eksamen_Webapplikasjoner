1582cb308189478f0739fa8a473118004fe29ec8
ramibakir <rami.bakir@hotmail.no>
2020-12-03 22:42:02 +0100

    Added labels and button with styling to contactform

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { StyledButton } from '../styles/MainStyles.js';
 import {
   StyledFormContainter,
   StyledForm,
   StyledInput,
   StyledTextArea,
+  StyledLabel,
 } from '../styles/FormStyles.js';
 
 const ContactForm = () => (
   <>
     <StyledFormContainter>
       <StyledForm>
+        <StyledLabel htmlFor="fullname">Fullt navn: </StyledLabel>
         <StyledInput required type="text" maxLength="100" />
+        <StyledLabel htmlFor="email">E-post: </StyledLabel>
         <StyledInput required type="email" />
-        <StyledTextArea />
+        <StyledLabel htmlFor="message">Din beskjed til oss: </StyledLabel>
+        <StyledTextArea wrap="off" required overflow="auto" />
+        <StyledButton>SEND BESKJED</StyledButton>
       </StyledForm>
     </StyledFormContainter>
   </>
