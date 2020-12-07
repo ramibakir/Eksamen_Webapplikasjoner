be92a3b86e7e868737540dd0bdcd61ff4ad53b86
ramibakir <rami.bakir@hotmail.no>
2020-12-07 14:14:34 +0100

    added articleSerivce and authSerivce

---
+++ client/src/utils/articleService.js
@@ -0,0 +1,42 @@
+import http from './http';
+
+const API_URL = 'articles';
+
+export const list = async () => {
+  try {
+    return await http.get(`${API_URL}`);
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const get = async (id) => {
+  try {
+    return await http.get(`${API_URL}/${id}`);
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const put = async (id, data) => {
+  try {
+    return await http.put(`${API_URL}/${id}`, data);
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const create = async (data) => {
+  try {
+    return await http.put(`${API_URL}`, data);
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export default {
+  list,
+  get,
+  put,
+  create,
+};
