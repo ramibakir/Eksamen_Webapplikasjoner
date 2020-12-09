a5818b2e6ac0112230ccc50b336cd03178f99fed
Ingrid <ingrid.e.dahl@hiof.no>
2020-12-09 22:31:09 +0100

    Pushing changes from server to client

--- server/middleware/validate.js
+++
@@ -1,8 +0,0 @@
-import { validationError } from '../utils/validation.js';
-
-export const validateFields = (schema) => async (req, res, next) => {
-  const validationErrors = await validationError(schema, req.body, res);
-  if (!validationErrors) {
-    next();
-  }
-};
