1fc113bf591cc7c738efd3e9724ce6b9a72a0a1c
Ingrid <ingrid.e.dahl@hiof.no>
2020-12-09 23:19:23 +0100

    copied content in userschema from server

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,1 +1,23 @@
-userschema;
+import Joi from 'joi';
+
+const userSchema = {
+  email: Joi.string().email().required().messages({
+    'any.required': 'Epost må fylles ut',
+    'string.email': 'Bruk riktig format på epost',
+    'string.empty': 'Fyll ut epost',
+  }),
+  password: Joi.string().min(8).required().messages({
+    'any.required': 'Passord må fylles ut',
+    'string.min': 'Må bestå av minst 8 tall og bokstaver',
+    'string.empty': 'Fyll ut passord',
+  }),
+};
+
+export const registerSchema = Joi.object().keys({
+  name: Joi.string(),
+  ...userSchema,
+}).options({ abortEarly: false });
+
+export const loginSchema = Joi.object().keys({
+  ...userSchema,
+}).options({ abortEarly: false });
