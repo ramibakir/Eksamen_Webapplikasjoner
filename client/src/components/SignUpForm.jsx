89c3ca1b9f18754165377c564ec2fe4ae83e8d1e
ramibakir <rami.bakir@hotmail.no>
2020-12-09 22:44:18 +0100

    added image field to article model together with a hidden field
    created signup form with signup functionality -> not yet tested. removed unnecessary methods from upload.jsx

---
+++ client/src/components/SignUpForm.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory, useLocation } from 'react-router-dom';
+import { StyledButton } from '../styles/mainStyles.js';
+import {
+  StyledFormContainter,
+  StyledForm,
+  StyledInput,
+  StyledLabel,
+} from '../styles/formStyles.js';
+import { create } from '../utils/userService';
+
+const SignUpForm = (props) => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = async (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const createUser = () => {
+    create(user);
+    props.history.push('/');
+  };
+
+  return (
+    <>
+      <StyledFormContainter>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledLabel htmlFor="name">Navn</StyledLabel>
+          <StyledInput
+            type="text"
+            required
+            placeholder="Fornavn og etternavn"
+            name="name"
+            value={user.name}
+            onChange={handleSubmit}
+          />
+
+          <StyledLabel htmlFor="email">Epost</StyledLabel>
+          <StyledInput
+            type="email"
+            required
+            placeholder="Din epost"
+            name="email"
+            value={user.email}
+            onChange={handleSubmit}
+          />
+
+          <StyledLabel htmlFor="password">Passord</StyledLabel>
+          <StyledInput
+            type="password"
+            required
+            placeholder="Ditt passord"
+            name="password"
+            value={user.password}
+            onChange={handleSubmit}
+          />
+
+          <StyledButton
+            style={{ margin: '30px 0 50px 0' }}
+            onClick={createUser}
+          >
+            REGISTRER DEG
+          </StyledButton>
+        </StyledForm>
+      </StyledFormContainter>
+    </>
+  );
+};
+
+export default SignUpForm;
